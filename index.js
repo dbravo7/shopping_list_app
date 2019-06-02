a944f9dadf6dff05ddf1ec9131e6447813efd6b9
David <dbravo7@yahoo.com>
2019-06-02 16:19:21 -0400

    Added questions in comments for reviewers

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // using the following to encompass all code: $(document).ready(() => { }. 
 // Is this standard practice?' 
 
+// Following code adds an item to the list 
+
   $('#js-shopping-list-form').submit(function(event) {
 
     "use strict";
@@ -31,13 +33,22 @@
   });
 // }
 
+// In the cat carousel assignment, only the 'click' handler is used but 
+// the key 'enter' also works in selecting items. However, I only used the 
+// handler 'click' here and the buttons in the web app are focused using 'tab', but 
+// spacebar or enter do not execute any function. Spacebar does cause the button
+// to change color, but does nothing else. Not sure what is different in my code
+// than the carousel cat assignment. 
+
+
+// Following code checks, unchecks and deletes items 
 
   $(document).on('click', '.button-label', function(event) {
 
     const parent_node = $(this).parent();
 
     if (parent_node.hasClass("shopping-item-toggle")) {
-      parent_node.closest("li").find('.shopping-item').toggleClass('shopping-item__checked');
+      parent_node.closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     } else if (parent_node.hasClass("shopping-item-delete")) {
       parent_node.closest("li").remove();
     }
